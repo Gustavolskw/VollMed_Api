743ac7f391645df077a4fd5044e7a09448751316
unknown <gustavolscmidt13@gmail.com>
2024-01-06 13:16:31 -0300

    Final projeot Alura

---
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsultas.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.medicos.Especialidade;
+import med.voll.api.domain.pacientes.Paciente;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsultas(Long id, Long idMedico, Long idPaciente, LocalDateTime data, Especialidade especialidade) {
+    public DadosDetalhamentoConsultas(Consulta consulta){
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData(), consulta.getMedico().getEspecialidade());
+    }
+}
